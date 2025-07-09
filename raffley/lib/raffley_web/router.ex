394f10d932807721f1e5a7f864f7becd33e6f915
Larry Jones <mrwizard82d1@gmail.com>
2025-07-09 15:50:29 -0500

    Create plug that adds a `greeting` to our pipeline
    Our `greeting` key can have any of the values:
    
    - Hi
    - Howdy
    - Hello

--- raffley/lib/raffley_web/router.ex
+++ raffley/lib/raffley_web/router.ex
@@ -8,10 +8,24 @@ defmodule RaffleyWeb.Router do
     plug :put_root_layout, html: {RaffleyWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :spy
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  def spy(conn, _opts) do
+    greeting =
+      ~w(Hi Howdy Hello)
+      |> Enum.random()
+
+    conn = assign(conn, :greeting, greeting)
+
+    # Avoid "polluting" the server log
+    # IO.inspect(conn)
+
+    conn
   end
 
   scope "/", RaffleyWeb do
