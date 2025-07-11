71b872504731fceb586913abd0fc96a75e18e42e
Larry Jones <mrwizard82d1@gmail.com>
2025-07-11 12:03:59 -0500

    Verify we can actually capture and render emojis

--- raffley/lib/raffley_web/controllers/rule_controller.ex
+++ raffley/lib/raffley_web/controllers/rule_controller.ex
@@ -2,6 +2,11 @@ defmodule RaffleyWeb.RuleController do
   use RaffleyWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    emojis =
+      ~w(\u{1f600} \u{1f917} \u{270c})
+      |> Enum.random()
+      |> String.duplicate(5)
+
+    render(conn, :index, emojis: emojis)
   end
 end
