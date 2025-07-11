ca9f779c7596034cfbae4c977f090effb2208b35
Larry Jones <mrwizard82d1@gmail.com>
2025-07-11 14:39:39 -0500

    Final rendering of tips

---
+++ heads_up/lib/heads_up/tips.ex
@@ -0,0 +1,19 @@
+defmodule Tips do
+
+  def list_tips() do
+     [
+      %{
+        id: 1,
+        text: "Slow is smooth, and smooth is fast!"
+      },
+       %{
+         id: 2,
+         text: "Working with a buddy is always a smart move."
+       },
+       %{
+         id: 3,
+         text: "Take it easy and enjoy!"
+       },
+     ]
+  end
+end
--- heads_up/lib/heads_up_web/controllers/tip_controller.ex
+++ heads_up/lib/heads_up_web/controllers/tip_controller.ex
@@ -2,6 +2,8 @@ defmodule HeadsUpWeb.TipController do
  use HeadsUpWeb, :controller
 
   def index(conn, _args) do
-    render(conn, :index)
+    tips = Tips.list_tips()
+
+    render(conn, :index, tips: tips)
   end
 end
