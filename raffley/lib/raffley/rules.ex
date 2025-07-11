941d054263dd80648f5a7060f4f265ee85798d4d
Larry Jones <mrwizard82d1@gmail.com>
2025-07-11 12:48:04 -0500

    Render our rules

---
+++ raffley/lib/raffley/rules.ex
@@ -0,0 +1,18 @@
+defmodule Raffley.Rules do
+  def list_rules() do
+    [
+      %{
+        id: 1,
+        text: "Participants must have a high tolerance for puns and dad jokes."
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claiming their prize."
+      },
+      %{
+        id: 3,
+        text: "Have fun!"
+      }
+    ]
+  end
+end
--- raffley/lib/raffley_web/controllers/rule_controller.ex
+++ raffley/lib/raffley_web/controllers/rule_controller.ex
@@ -1,12 +1,16 @@
 defmodule RaffleyWeb.RuleController do
   use RaffleyWeb, :controller
 
+  alias Raffley.Rules
+
   def index(conn, _params) do
     emojis =
       ~w(\u{1f600} \u{1f917} \u{270c})
       |> Enum.random()
       |> String.duplicate(5)
 
-    render(conn, :index, emojis: emojis)
+    rules = Rules.list_rules()
+
+    render(conn, :index, emojis: emojis, rules: rules)
   end
 end
