b14217b0a463ad6569fe9999672b2a25e3228672
Larry Jones <mrwizard82d1@gmail.com>
2025-07-12 12:48:24 -0500

    Display assigned greeting when displaying rule
    The greeting "binding" was added to the `conn`(ection) as part of the
    `spy` plug.

--- raffley/lib/raffley_web/controllers/rule_html.ex
+++ raffley/lib/raffley_web/controllers/rule_html.ex
@@ -6,7 +6,7 @@ defmodule RaffleyWeb.RuleHTML do
   def show(assigns) do
     ~H"""
     <div class="rules">
-      <h1>Don't forget...</h1>
+      <h1>{@greeting}! Don't forget...</h1>
       <p>{@rule.text}</p>
     </div>
     """
