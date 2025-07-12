8d8b7c14491cc166085e7183b7822084ea006e8f
Larry Jones <mrwizard82d1@gmail.com>
2025-07-12 12:25:13 -0500

    Display a specific rule if queried

--- raffley/lib/raffley_web/controllers/rule_controller.ex
+++ raffley/lib/raffley_web/controllers/rule_controller.ex
@@ -13,4 +13,9 @@ defmodule RaffleyWeb.RuleController do
 
     render(conn, :index, emojis: emojis, rules: rules)
   end
+
+  def show(conn, %{"id" => id}) do
+    rule = Rules.get_rule(id)
+    render(conn, :show, rule: rule)
+  end
 end
