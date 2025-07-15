039ccdd7899d24b06c39d6a24b405d5c96866d19
Larry Jones <mrwizard82d1@gmail.com>
2025-07-15 11:59:02 -0500

    Revert "Use heex template to "automagically" render page"
    This reverts commit 5dfb9f29b77a14c8393e90d5d90c906f6bf659c8.

--- raffley/lib/raffley_web/live/estimator_live.ex
+++ raffley/lib/raffley_web/live/estimator_live.ex
@@ -2,16 +2,13 @@ defmodule RaffleyWeb.EstimatorLive do
   use RaffleyWeb, :live_view
 
   # A LiveView module defines three primary callback functions
-  # `mount/3`
-  # `render/1`
+  # `mount`
+  # `render`
   # `handel_event`
 
   def mount(_params, _session, socket) do
     # Assigns two pieces of "state" for our "socket"
     socket = assign(socket, tickets: 0, price: 3)
-
-    # What does a socket look like?
-    IO.inspect(socket)
 
     # Must return a two-item tuple
     {:ok, socket}
@@ -20,7 +17,27 @@ defmodule RaffleyWeb.EstimatorLive do
     # {:ok, assign(socket, tickets: 0, price: 3)}
   end
 
-  # By moving / including a heex template, in this case,
-  # `estimator_live.html.heex`, we can use the `render/1` function provided
-  # by the framework (I believe).
+  def render(assigns) do
+    # Returns a HEEx template
+    # Here, we use the inline sigil
+    ~H"""
+    <div class="estimator">
+      <h1>Raffle Estimator</h1>
+
+      <section>
+        <div>
+          {@tickets}
+        </div>
+        @
+        <div>
+          {@price}
+        </div>
+        =
+        <div>
+          ${@tickets * @price}
+        </div>
+      </section>
+    </div>
+    """
+  end
 end
