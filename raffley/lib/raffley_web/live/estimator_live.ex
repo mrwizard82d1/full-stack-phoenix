5dfb9f29b77a14c8393e90d5d90c906f6bf659c8
Larry Jones <mrwizard82d1@gmail.com>
2025-07-15 11:57:37 -0500

    Use heex template to "automagically" render page
    At least for now...

--- raffley/lib/raffley_web/live/estimator_live.ex
+++ raffley/lib/raffley_web/live/estimator_live.ex
@@ -2,13 +2,16 @@ defmodule RaffleyWeb.EstimatorLive do
   use RaffleyWeb, :live_view
 
   # A LiveView module defines three primary callback functions
-  # `mount`
-  # `render`
+  # `mount/3`
+  # `render/1`
   # `handel_event`
 
   def mount(_params, _session, socket) do
     # Assigns two pieces of "state" for our "socket"
     socket = assign(socket, tickets: 0, price: 3)
+
+    # What does a socket look like?
+    IO.inspect(socket)
 
     # Must return a two-item tuple
     {:ok, socket}
@@ -17,27 +20,7 @@ defmodule RaffleyWeb.EstimatorLive do
     # {:ok, assign(socket, tickets: 0, price: 3)}
   end
 
-  def render(assigns) do
-    # Returns a HEEx template
-    # Here, we use the inline sigil
-    ~H"""
-    <div class="estimator">
-      <h1>Raffle Estimator</h1>
-
-      <section>
-        <div>
-          {@tickets}
-        </div>
-        @
-        <div>
-          {@price}
-        </div>
-        =
-        <div>
-          ${@tickets * @price}
-        </div>
-      </section>
-    </div>
-    """
-  end
+  # By moving / including a heex template, in this case,
+  # `estimator_live.html.heex`, we can use the `render/1` function provided
+  # by the framework (I believe).
 end
