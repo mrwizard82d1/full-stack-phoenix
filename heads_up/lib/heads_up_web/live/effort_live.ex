c0585c284ae49343a718555712891ee1ae1721e5
Larry Jones <mrwizard82d1@gmail.com>
2025-07-17 11:54:01 -0500

    Inspect pid at mount, render, and handle event

--- heads_up/lib/heads_up_web/live/effort_live.ex
+++ heads_up/lib/heads_up_web/live/effort_live.ex
@@ -2,10 +2,14 @@ defmodule HeadsUpWeb.EffortLive do
   use HeadsUpWeb, :live_view
 
   def mount(_params, _session, socket) do
+    IO.inspect(self(), label: "mount/3")
+
     {:ok, assign(socket, responders: 0, minutes_per_responder: 10)}
   end
 
   def render(assigns) do
+    IO.inspect(self(), label: "render/1")
+
     ~H"""
     <div class="effort">
       <h1>Community Love</h1>
@@ -30,6 +34,8 @@ defmodule HeadsUpWeb.EffortLive do
   end
 
   def handle_event("add", %{"number" => number}, socket) do
+    IO.inspect(self(), label: "handle_event/1 (add)")
+
     socket = update(socket, :responders, &(&1 + String.to_integer(number)))
 
     IO.inspect(socket, label: "socket")
