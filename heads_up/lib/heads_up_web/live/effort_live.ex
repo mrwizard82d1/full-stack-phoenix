d4409675a990515054648d66ec504ad349af9444
Larry Jones <mrwizard82d1@gmail.com>
2025-07-17 11:56:28 -0500

    Revert "Raise event ("Kapow!") for automatic restart"
    This reverts commit 4b5f7b648ae939a3e0044c63f7c3d303442eea04.

--- heads_up/lib/heads_up_web/live/effort_live.ex
+++ heads_up/lib/heads_up_web/live/effort_live.ex
@@ -36,8 +36,6 @@ defmodule HeadsUpWeb.EffortLive do
   def handle_event("add", %{"number" => number}, socket) do
     IO.inspect(self(), label: "handle_event/1 (add)")
 
-    raise("Kapow!")
-
     socket = update(socket, :responders, &(&1 + String.to_integer(number)))
 
     IO.inspect(socket, label: "socket")
