15ce7896dd2dbb5d48b19f5b491d9574bc4a3773
Larry Jones <mrwizard82d1@gmail.com>
2025-07-22 11:21:24 -0500

    Continuing to refactor code
    Extract HTML for a raffle ticket into its own function

--- raffley/lib/raffley_web/live/raffle_live/index.ex
+++ raffley/lib/raffley_web/live/raffle_live/index.ex
@@ -12,16 +12,22 @@ defmodule RaffleyWeb.RaffleLive.Index do
     ~H"""
     <div class="raffle-index">
       <div class="raffles">
-        <div :for={raffle <- @raffles} class="card">
-          <img src={raffle.image_path} />
-          <h2>{raffle.prize}</h2>
-          <div class="details">
-            <div class="price">
-              ${raffle.ticket_price} / ticket
-            </div>
-            <.badge raffle={raffle} />
-          </div>
+        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+      </div>
+    </div>
+    """
+  end
+
+  def raffle_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@raffle.image_path} />
+      <h2>{@raffle.prize}</h2>
+      <div class="details">
+        <div class="price">
+          ${@raffle.ticket_price} / ticket
         </div>
+        <.badge status={@raffle.status} />
       </div>
     </div>
     """
@@ -30,7 +36,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
   def badge(assigns) do
     ~H"""
     <div class="badge">
-      {@raffle.status}
+      {@status}
     </div>
     """
   end
