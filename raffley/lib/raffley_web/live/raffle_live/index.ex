634281cf710edf145721093d07e1b2818bde6901
Larry Jones <mrwizard82d1@gmail.com>
2025-07-22 11:38:48 -0500

    Add expected `:values` to `attr``
    This change, again, produces a warning with we change the call by
    passing, for example, `:mike`, instead of the `status`

--- raffley/lib/raffley_web/live/raffle_live/index.ex
+++ raffley/lib/raffley_web/live/raffle_live/index.ex
@@ -27,7 +27,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
         <div class="price">
           ${@raffle.ticket_price} / ticket
         </div>
-        <.badge status={@raffle.status} />
+        <.badge status={:mike} />
       </div>
     </div>
     """
@@ -37,7 +37,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
   # function results in a "compile time issue." Notice the "squiggle"
   # at the beginning of the `.badge` "tag". Additionally, this
   # attribute raises a **compiler error**.
-  attr :status, :atom, required: true
+  attr :status, :atom, required: true, values: [:upcoming, :open, :closed]
 
   def badge(assigns) do
     ~H"""
