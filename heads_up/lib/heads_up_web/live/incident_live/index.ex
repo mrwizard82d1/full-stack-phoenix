c7a3eee8794ad20ade4308da66da5be4a2b0c2be
Larry Jones <mrwizard82d1@gmail.com>
2025-07-24 14:44:28 -0500

    Extract incident_card and badge as components

--- heads_up/lib/heads_up_web/live/incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/incident_live/index.ex
@@ -12,21 +12,32 @@ defmodule HeadsUpWeb.IncidentLive.Index do
     ~H"""
     <div class="incident-index">
       <div class="incidents">
-        <div :for={incident <- @incidents} class="card">
-          <img src={incident.image_path} />
-          <h2>{incident.name}</h2>
-          <div class="details">
-            <div class="badge">
-              {incident.status}
-            </div>
-            <div class="priority">
-              {incident.priority}
-            </div>
-          </div>
+        <.incident_card :for={incident <- @incidents} incident={incident} />
+      </div>
+    </div>
+    """
+  end
+
+  def incident_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@incident.image_path} />
+      <h2>{@incident.name}</h2>
+      <div class="details">
+        <.badge status={@incident.status} />
+        <div class="priority">
+          {@incident.priority}
         </div>
       </div>
     </div>
     """
   end
 
+  def badge(assigns) do
+    ~H"""
+    <div class="badge">
+      {@status}
+    </div>
+    """
+  end
 end
