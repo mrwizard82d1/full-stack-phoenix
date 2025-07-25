84688fc8509fec4eb13c0b88b38da431d7ad25f9
Larry Jones <mrwizard82d1@gmail.com>
2025-07-25 11:47:52 -0500

    Extract badge component into CustomComponents

---
+++ raffley/lib/raffley_web/components/custom_components.ex
@@ -0,0 +1,24 @@
+defmodule RaffleyWeb.CustomComponents do
+  use RaffleyWeb, :html
+
+  # NOTE: Defining a required attribute on a structure passed to a
+  # function results in a "compile time issue." Notice the "squiggle"
+  # at the beginning of the `.badge` "tag". Additionally, this
+  # attribute raises a **compiler error**.
+  attr :status, :atom, values: [:upcoming, :open, :closed], default: :upcoming
+  attr :class, :string, default: nil
+
+  def badge(assigns) do
+    ~H"""
+    <div class={[
+      "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
+      @status == :open && "text-lime-600 border-lime-600",
+      @status == :upcoming && "text-amber-600 border-amber-600",
+      @status == :closed && "text-gray-600 border-gray-600",
+      @class
+    ]}>
+      {@status}
+    </div>
+    """
+  end
+end
--- raffley/lib/raffley_web/live/raffle_live/index.ex
+++ raffley/lib/raffley_web/live/raffle_live/index.ex
@@ -2,6 +2,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
   use RaffleyWeb, :live_view
 
   alias Raffley.Raffles
+  alias RaffleyWeb.CustomComponents
 
   def mount(_params, _session, socket) do
     socket = assign(socket, :raffles, Raffles.list_raffles())
@@ -29,29 +30,8 @@ defmodule RaffleyWeb.RaffleLive.Index do
         <div class="price">
           ${@raffle.ticket_price} / ticket
         </div>
-        <.badge status={@raffle.status} class="animate-spin" />
+        <CustomComponents.badge status={@raffle.status} />
       </div>
-    </div>
-    """
-  end
-
-  # NOTE: Defining a required attribute on a structure passed to a
-  # function results in a "compile time issue." Notice the "squiggle"
-  # at the beginning of the `.badge` "tag". Additionally, this
-  # attribute raises a **compiler error**.
-  attr :status, :atom, values: [:upcoming, :open, :closed], default: :upcoming
-  attr :class, :string, default: nil
-
-  def badge(assigns) do
-    ~H"""
-    <div class={[
-      "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
-      @status == :open && "text-lime-600 border-lime-600",
-      @status == :upcoming && "text-amber-600 border-amber-600",
-      @status == :closed && "text-gray-600 border-gray-600",
-      @class
-    ]}>
-      {@status}
     </div>
     """
   end
