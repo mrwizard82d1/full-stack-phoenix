df2fd4927323d9c0fe5383e23b3e89afb1dc139c
Larry Jones <mrwizard82d1@gmail.com>
2025-07-25 12:07:38 -0500

    Extract `badge` into CustomComponents module

--- heads_up/lib/heads_up_web/live/incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/incident_live/index.ex
@@ -2,6 +2,7 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   use HeadsUpWeb, :live_view
 
   alias HeadsUp.Incidents
+  alias HeadsUpWeb.CustomComponents
 
   def mount(_params, _session, socket) do
     socket = assign(socket, :incidents, Incidents.list_incidents())
@@ -26,26 +27,11 @@ defmodule HeadsUpWeb.IncidentLive.Index do
       <img src={@incident.image_path} />
       <h2>{@incident.name}</h2>
       <div class="details">
-        <.badge status={@incident.status} />
+        <CustomComponents.badge status={@incident.status} />
         <div class="priority">
           {@incident.priority}
         </div>
       </div>
-    </div>
-    """
-  end
-
-  attr :status, :atom, values: [:pending, :resolved, :canceled], default: :pending
-
-  def badge(assigns) do
-    ~H"""
-    <div class={[
-      "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
-      @status == :resolved && "text-lime-600 border-lime-600",
-      @status == :pending && "text-amber-600 border-amber-600",
-      @status == :canceled && "text-gray-600 border-gray-600"
-    ]}>
-      {@status}
     </div>
     """
   end
