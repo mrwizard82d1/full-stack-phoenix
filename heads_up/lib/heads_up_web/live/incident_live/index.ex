0bbe802901b6ac549c9f53bf05987e5e00fb6eaf
Larry Jones <mrwizard82d1@gmail.com>
2025-07-25 12:02:06 -0500

    Dynamically determine badeg "color"

--- heads_up/lib/heads_up_web/live/incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/incident_live/index.ex
@@ -41,7 +41,9 @@ defmodule HeadsUpWeb.IncidentLive.Index do
     ~H"""
     <div class={[
       "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
-      "text-lime-600 border-lime-600"
+      @status == :resolved && "text-lime-600 border-lime-600",
+      @status == :pending && "text-amber-600 border-amber-600",
+      @status == :canceled && "text-gray-600 border-gray-600"
     ]}>
       {@status}
     </div>
