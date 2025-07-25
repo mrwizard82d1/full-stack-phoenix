655aca00b839539e7f9a73db9860a46bc4d0f85c
Larry Jones <mrwizard82d1@gmail.com>
2025-07-25 11:33:48 -0500

    Change status calor based on its value

--- raffley/lib/raffley_web/live/raffle_live/index.ex
+++ raffley/lib/raffley_web/live/raffle_live/index.ex
@@ -45,7 +45,9 @@ defmodule RaffleyWeb.RaffleLive.Index do
     ~H"""
     <div class={[
       "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
-      "text-lime-600 border-lime-600"
+      @status == :open && "text-lime-600 border-lime-600",
+      @status == :upcoming && "text-amber-600 border-amber-600",
+      @status == :closed && "text-grey-600 border-grey-600"
     ]}>
       {@status}
     </div>
