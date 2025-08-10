13443a9e5e94fda13e8428e9d5751efed4ea1c5e
Larry Jones <mrwizard82d1@gmail.com>
2025-08-10 14:31:41 -0500

    First steps moving to Ecto (PostgreSQL) database

--- raffley/lib/raffley/raffles/raffle.ex
+++ raffley/lib/raffley/raffles/raffle.ex
@@ -3,11 +3,14 @@ defmodule Raffley.Raffles.Raffle do
   import Ecto.Changeset
 
   schema "raffles" do
-    field :status, Ecto.Enum, values: [:upcoming, :open, :closed]
+    field :status, Ecto.Enum,
+      values: [:upcoming, :open, :closed],
+      default: :upcoming
+
     field :description, :string
     field :prize, :string
-    field :ticket_price, :integer
-    field :image_path, :string
+    field :ticket_price, :integer, default: 1
+    field :image_path, :string, default: "/images/placeholder.jpg"
 
     timestamps(type: :utc_datetime)
   end
