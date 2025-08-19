7f569078409d9dea73109200787a532454937506
Larry Jones <mrwizard82d1@gmail.com>
2025-08-19 12:46:10 -0500

    Inspect DOM at mount and after render
    This action provides instrumentation on the request/response lifecycle.

--- raffley/lib/raffley_web/live/raffle_live/index.ex
+++ raffley/lib/raffley_web/live/raffle_live/index.ex
@@ -6,6 +6,15 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
   def mount(_params, _session, socket) do
     socket = stream(socket, :raffles, Raffles.list_raffles())
+
+    IO.inspect(socket.assigns.streams.raffles, label: "MOUNT")
+
+    socket =
+      attach_hook(socket, :log_stream, :after_render, fn socket ->
+        IO.inspect(socket.assigns.streams.raffles, label: "AFTER RENDER")
+        socket
+      end)
+
     {:ok, socket}
   end
 
