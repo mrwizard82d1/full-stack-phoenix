7e499e51192856637c013683a92f364ca735fa6a
Larry Jones <mrwizard82d1@gmail.com>
2025-08-20 14:32:14 -0500

    Change `featured_raffles` to use query and limit

--- raffley/lib/raffley/raffles.ex
+++ raffley/lib/raffley/raffles.ex
@@ -21,7 +21,11 @@ defmodule Raffley.Raffles do
   end
 
   def featured_raffles(raffle) do
-    list_raffles()
-    |> List.delete(raffle)
+    Raffle
+    |> where(status: :open)
+    |> where([r], r.id != ^raffle.id)
+    |> order_by(desc: :ticket_price)
+    |> limit(3)
+    |> Repo.all()
   end
 end
