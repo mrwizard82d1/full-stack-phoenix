1f6f9e713c2f3ec318ae7d748b3fe5492c887357
Larry Jones <mrwizard82d1@gmail.com>
2025-08-24 22:44:59 -0500

    Add debounce to search
    Begin with 1 second (1000 ms) to more easily see debounce in action.

--- raffley/lib/raffley_web/live/raffle_live/index.ex
+++ raffley/lib/raffley_web/live/raffle_live/index.ex
@@ -46,7 +46,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
   def filter_form(assigns) do
     ~H"""
     <.form for={@form} id="filter-form" phx-change="filter">
-      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" />
+      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" phx-debounce="1000" />
 
       <.input
         type="select"
