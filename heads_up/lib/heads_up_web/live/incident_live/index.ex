1d86d0f69360a6f924fccdd9e3633fac13483cc6
Larry Jones <mrwizard82d1@gmail.com>
2025-08-26 13:12:10 -0500

    Add calls to `IO.inspect` to verify no mount

--- heads_up/lib/heads_up_web/live/incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/incident_live/index.ex
@@ -5,6 +5,8 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   alias HeadsUpWeb.CustomComponents
 
   def mount(_params, _session, socket) do
+    IO.inspect(self(), label: "MOUNT")
+
     # IO.inspect(socket.assigns.streams.incidents, label: "MOUNT")
 
     # socket =
@@ -17,6 +19,8 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   end
 
   def handle_params(params, _uri, socket) do
+    IO.inspect(self(), label: "HANDLE PARAMS")
+
     socket =
       socket
       |> stream(:incidents, Incidents.filter_incidents(params),
@@ -29,6 +33,8 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   end
 
   def render(assigns) do
+    IO.inspect(self(), label: "RENDER")
+
     ~H"""
     <div class="incident-index">
       <div class="headline">
