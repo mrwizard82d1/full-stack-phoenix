22411cc3d6244ff9a92a2f7562334b94eaadbda4
Larry Jones <mrwizard82d1@gmail.com>
2025-08-27 13:50:15 -0500

    Handle async failure fetching featured raffles

--- raffley/lib/raffley_web/live/raffle_live/show.ex
+++ raffley/lib/raffley_web/live/raffle_live/show.ex
@@ -28,9 +28,6 @@ defmodule RaffleyWeb.RaffleLive.Show do
 
   def render(assigns) do
     ~H"""
-    <pre>
-      {inspect(@featured_raffles, pretty: true)}
-    </pre>
     <div class="raffle-show">
       <div class="raffle">
         <img src={@raffle.image_path} />
@@ -64,17 +61,10 @@ defmodule RaffleyWeb.RaffleLive.Show do
       <div :if={@raffles.loading} class="loading">
         <div class="spinner"></div>
       </div>
-      <!--
-      Because `@raffles` is an asynchronous result, we need to check if
-      it has been resolved **before** attempting to render each
-      featured raffle.
-      -->
+      <div :if={@raffles.failed} class="faile">
+        Yikes!
+      </div>
       <ul :if={@raffles.ok?} class="raffles">
-        <!--
-        Because we are fetching the raffles asynchronously, we now query
-        the asynchronous result of the fetch. This action will implicitly
-        await the asynchronous result.
-        -->
         <li :for={raffle <- @raffles.result}>
           <.link navigate={~p"/raffles/#{raffle}"}>
             <img src={raffle.image_path} /> {raffle.prize}
