2abdf6de4b7286e44b3b11ef17a8288d7e4ca1a1
Larry Jones <mrwizard82d1@gmail.com>
2025-09-02 12:28:56 -0500

    Accept submission of a new incident
    But all is not "quiet and peaceful" in the 'hood.

--- heads_up/lib/heads_up_web/live/admin_incident_live/form.ex
+++ heads_up/lib/heads_up_web/live/admin_incident_live/form.ex
@@ -1,6 +1,8 @@
 defmodule HeadsUpWeb.AdminIncidentLive.Form do
   use HeadsUpWeb, :live_view
 
+  alias HeadsUp.Admin
+
   def mount(_params, _session, socket) do
     socket =
       socket
@@ -15,7 +17,7 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
     <.header>
       {@page_title}
     </.header>
-    <.simple_form for={@form} id="incident-form">
+    <.simple_form for={@form} id="incident-form" phx-submit="save">
       <.input field={@form[:name]} label="Name" />
       <.input field={@form[:description]} label="Description" />
       <.input field={@form[:priority]} type="number" label="Priority" />
@@ -28,12 +30,20 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
       />
       <.input field={@form[:image_path]} label="Image Path" />
       <:actions>
-        <.button>Save Incident</.button>
+        <.button phx-disable-with="Saving...">Save Incident</.button>
       </:actions>
     </.simple_form>
     <.back navigate={~p"/admin/incidents"}>
       Back
     </.back>
     """
+  end
+
+  def handle_event("save", %{"incident" => incident_params}, socket) do
+    Admin.create_incident(incident_params)
+
+    socket = push_navigate(socket, to: ~p"/admin/incidents")
+
+    {:noreply, socket}
   end
 end
