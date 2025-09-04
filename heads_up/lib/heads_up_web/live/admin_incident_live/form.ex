dfff9f56ab825f9c990e817339db7855442a0264
Larry Jones <mrwizard82d1@gmail.com>
2025-09-04 13:30:49 -0500

    Validation using change sets

--- heads_up/lib/heads_up_web/live/admin_incident_live/form.ex
+++ heads_up/lib/heads_up_web/live/admin_incident_live/form.ex
@@ -2,12 +2,15 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   use HeadsUpWeb, :live_view
 
   alias HeadsUp.Admin
+  alias HeadsUp.Incidents.Incident
 
   def mount(_params, _session, socket) do
+    changeset = Incident.changeset(%Incident{}, %{})
+
     socket =
       socket
       |> assign(:page_title, "New Incident")
-      |> assign(:form, to_form(%{}, as: "incident"))
+      |> assign(:form, to_form(changeset))
 
     {:ok, socket}
   end
@@ -40,10 +43,17 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   end
 
   def handle_event("save", %{"incident" => incident_params}, socket) do
-    Admin.create_incident(incident_params)
+    case Admin.create_incident(incident_params) do
+      {:ok, _incident} ->
+        socket =
+          socket
+          |> put_flash(:info, "Incident created successfully")
+          |> push_navigate(to: ~p"/admin/incidents")
 
-    socket = push_navigate(socket, to: ~p"/admin/incidents")
+        {:noreply, socket}
 
-    {:noreply, socket}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
   end
 end
