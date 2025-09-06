e9da79d8cd27a008a2e0f08627a039cb983e3916
Larry Jones <mrwizard82d1@gmail.com>
2025-09-06 15:41:42 -0500

    "Finish" edit incidents

--- heads_up/lib/heads_up_web/live/admin_incident_live/form.ex
+++ heads_up/lib/heads_up_web/live/admin_incident_live/form.ex
@@ -4,15 +4,30 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   alias HeadsUp.Admin
   alias HeadsUp.Incidents.Incident
 
-  def mount(_params, _session, socket) do
-    changeset = Admin.change_incident(%Incident{})
+  def mount(params, _session, socket) do
+    {:ok, apply_action(socket, socket.assigns.live_action, params)}
+  end
 
-    socket =
-      socket
-      |> assign(:page_title, "New Incident")
-      |> assign(:form, to_form(changeset))
+  defp apply_action(socket, :new, _params) do
+    incident = %Incident{}
 
-    {:ok, socket}
+    changeset = Admin.change_incident(incident)
+
+    socket
+    |> assign(:page_title, "New Incident")
+    |> assign(:form, to_form(changeset))
+    |> assign(:incident, incident)
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    incident = Admin.get_incident!(id)
+
+    changeset = Admin.change_incident(incident)
+
+    socket
+    |> assign(:page_title, "Edit Incident")
+    |> assign(:form, to_form(changeset))
+    |> assign(:incident, incident)
   end
 
   def render(assigns) do
