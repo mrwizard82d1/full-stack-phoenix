0c743ccbf8d1c5ac518f203116cf2ffb7ea57415
Larry Jones <mrwizard82d1@gmail.com>
2025-09-08 12:41:40 -0500

    Delete incidents from the database (basics)

--- heads_up/lib/heads_up_web/live/admin_incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/admin_incident_live/index.ex
@@ -41,8 +41,20 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
             Edit
           </.link>
         </:action>
+        <:action :let={{_dom_id, incident}}>
+          <.link phx-click="delete" phx-value-id={incident.id} data-confirm="Are you sure?">
+            Delete
+          </.link>
+        </:action>
       </.table>
     </div>
     """
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    incident = Admin.get_incident!(id)
+    {:ok, _} = Admin.delete_incident(incident)
+
+    {:noreply, stream_delete(socket, :incidents, incident)}
   end
 end
