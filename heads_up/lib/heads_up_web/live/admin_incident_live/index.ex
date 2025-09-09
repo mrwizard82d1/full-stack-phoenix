e3c34ab10c883203312baa2d4dc96d6d3e12579b
Larry Jones <mrwizard82d1@gmail.com>
2025-09-09 15:39:56 -0500

    Module 39 exercises

--- heads_up/lib/heads_up_web/live/admin_incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/admin_incident_live/index.ex
@@ -16,6 +16,18 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
   def render(assigns) do
     ~H"""
     <div class="admin-index">
+      <.button phx-click={
+        JS.toggle(
+          to: "#joke",
+          in: "fade-in",
+          out: "fade-out"
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div id="joke" class="joke hidden" phx-click={JS.toggle_class("blur")}>
+        Why shouldn't you trust trees?
+      </div>
       <.header>
         {@page_title}
         <:actions>
@@ -24,7 +36,11 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
           </.link>
         </:actions>
       </.header>
-      <.table id="incidents" rows={@streams.incidents}>
+      <.table
+        id="incidents"
+        rows={@streams.incidents}
+        row_click={fn {_id, incident} -> JS.navigate(~p"/incidents/#{incident}") end}
+      >
         <:col :let={{_dom_id, incident}} label="Name">
           <.link navigate={~p"/incidents/#{incident}"}>
             {incident.name}
@@ -41,8 +57,8 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
             Edit
           </.link>
         </:action>
-        <:action :let={{_dom_id, incident}}>
-          <.link phx-click="delete" phx-value-id={incident.id} data-confirm="Are you sure?">
+        <:action :let={{dom_id, incident}}>
+          <.link phx-click={delete_and_hide(dom_id, incident)} data-confirm="Are you sure?">
             <.icon name="hero-trash" class="h-4 w-4" />
           </.link>
         </:action>
@@ -56,5 +72,10 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
     {:ok, _} = Admin.delete_incident(incident)
 
     {:noreply, stream_delete(socket, :incidents, incident)}
+  end
+
+  def delete_and_hide(dom_id, incident) do
+    JS.push("delete", value: %{id: incident.id})
+    |> JS.hide(to: "##{dom_id}", transition: "fade-out")
   end
 end
