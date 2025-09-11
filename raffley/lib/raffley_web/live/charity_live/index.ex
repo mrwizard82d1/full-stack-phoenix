e8ffaf6e01d86c74f02b41c47c78fcc3ea72d271
Larry Jones <mrwizard82d1@gmail.com>
2025-09-11 12:02:34 -0500

    Revert "An alternative to the generated code"
    This reverts commit c3bd3e630678b036fd7fc4563190d8a8fb329d3d.

--- raffley/lib/raffley_web/live/charity_live/index.ex
+++ raffley/lib/raffley_web/live/charity_live/index.ex
@@ -9,8 +9,10 @@ defmodule RaffleyWeb.CharityLive.Index do
     <.header>
       Listing Charities
       <:actions>
-        <.button phx-click={JS.navigate(~p"/charities/new")}>
-          New Charity
+        <.button phx-click={JS.dispatch("click", to: {:inner, "a"})}>
+          <.link navigate={~p"/charities/new"}>
+            New Charity
+          </.link>
         </.button>
       </:actions>
     </.header>
