57ea352875f4202b0a1a24676987878a9dbb8c3b
Larry Jones <mrwizard82d1@gmail.com>
2025-09-13 19:15:36 -0500

    Change seed file to include charities

--- raffley/priv/repo/seeds.exs
+++ raffley/priv/repo/seeds.exs
@@ -12,12 +12,23 @@
 
 alias Raffley.Repo
 alias Raffley.Raffles.Raffle
-alias Raffle.Charities.Charity
+alias Raffley.Charities.Charity
 
-# Create a charity and assign it to a variable.
-#
-# We explain the reason momentarily.
-hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
+wildlife =
+  %Charity{name: "Wildlife Refuge", slug: "wildlife-refuge"}
+  |> Repo.insert!()
+
+food =
+  %Charity{name: "Food Pantry", slug: "food-pantry"}
+  |> Repo.insert!()
+
+hope =
+  %Charity{name: "Homes of Hope", slug: "homes-of-hope"}
+  |> Repo.insert!()
+
+grace =
+  %Charity{name: "Grace Foundation", slug: "grace-foundation"}
+  |> Repo.insert!()
 
 %Raffle{
   prize: "Autographed Jersey",
@@ -38,7 +49,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 3,
   status: :upcoming,
-  image_path: "/images/yeti-coffee.jpg"
+  image_path: "/images/yeti-coffee.jpg",
+  charity: wildlife
 }
 |> Repo.insert!()
 
@@ -49,7 +61,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 1,
   status: :closed,
-  image_path: "/images/comic-book.jpg"
+  image_path: "/images/comic-book.jpg",
+  charity: food
 }
 |> Repo.insert!()
 
@@ -60,7 +73,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 3,
   status: :open,
-  image_path: "/images/dinner-for-two.jpg"
+  image_path: "/images/dinner-for-two.jpg",
+  charity: hope
 }
 |> Repo.insert!()
 
@@ -71,7 +85,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 2,
   status: :open,
-  image_path: "/images/cooking-class.jpg"
+  image_path: "/images/cooking-class.jpg",
+  charity: food
 }
 |> Repo.insert!()
 
@@ -82,7 +97,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 3,
   status: :upcoming,
-  image_path: "/images/basketball-tickets.jpg"
+  image_path: "/images/basketball-tickets.jpg",
+  charity: grace
 }
 |> Repo.insert!()
 
@@ -93,7 +109,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 1,
   status: :closed,
-  image_path: "/images/chocolate-basket.jpg"
+  image_path: "/images/chocolate-basket.jpg",
+  charity: food
 }
 |> Repo.insert!()
 
@@ -104,7 +121,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 2,
   status: :open,
-  image_path: "/images/pet-photo.jpg"
+  image_path: "/images/pet-photo.jpg",
+  charity: grace
 }
 |> Repo.insert!()
 
@@ -115,7 +133,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 3,
   status: :upcoming,
-  image_path: "/images/swamp-boat-ride.jpg"
+  image_path: "/images/swamp-boat-ride.jpg",
+  charity: wildlife
 }
 |> Repo.insert!()
 
@@ -126,7 +145,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 3,
   status: :upcoming,
-  image_path: "/images/race-car.jpg"
+  image_path: "/images/race-car.jpg",
+  charity: grace
 }
 |> Repo.insert!()
 
@@ -137,7 +157,8 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 2,
   status: :closed,
-  image_path: "/images/coffee.jpg"
+  image_path: "/images/coffee.jpg",
+  charity: hope
 }
 |> Repo.insert!()
 
@@ -148,6 +169,7 @@ hope = %Charity{name: "Homes of Hope", slug: "homes-of-hope"} |> Repo.insert!()
   """,
   ticket_price: 1,
   status: :upcoming,
-  image_path: "/images/balloon-ride.jpg"
+  image_path: "/images/balloon-ride.jpg",
+  charity: wildlife
 }
 |> Repo.insert!()
