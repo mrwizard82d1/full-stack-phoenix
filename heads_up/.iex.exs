3e78552e34aa18095db6e8db01e75a28b9c4cddd
Larry Jones <mrwizard82d1@gmail.com>
2025-09-17 15:45:14 -0500

    Add alias for Category for convenience

--- heads_up/.iex.exs
+++ heads_up/.iex.exs
@@ -1,7 +1,8 @@
 alias HeadsUp.Admin
-alias HeadsUp.Repo
+alias HeadsUp.Categories.Category
 alias HeadsUp.Incidents
 alias HeadsUp.Incidents.Incident
+alias HeadsUp.Repo
 
 # Easily query our database
 import Ecto.Query
