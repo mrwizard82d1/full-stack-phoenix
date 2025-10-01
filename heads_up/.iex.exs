d337fed724fd8a2f4cfe48305af9064e15b8309b
Larry Jones <mrwizard82d1@gmail.com>
2025-10-01 12:48:15 -0500

    Add :username to :users table and migrate
    Also delete all existing users.

--- heads_up/.iex.exs
+++ heads_up/.iex.exs
@@ -1,9 +1,10 @@
+alias HeadsUp.Accounts.User
 alias HeadsUp.Admin
 alias HeadsUp.Categories.Category
 alias HeadsUp.Incidents
 alias HeadsUp.Incidents.Incident
 alias HeadsUp.Repo
 
-# Easily query our database
+# Easily query our databasel
 import Ecto.Query
 import Ecto.Changeset
