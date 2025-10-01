537e11b9c4ea8a8a339d893188eb599b65d9f410
Larry Jones <mrwizard82d1@gmail.com>
2025-10-01 12:35:41 -0500

    Comment out inspection: `incident_card` `assigns`

--- heads_up/lib/heads_up_web/live/incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/incident_live/index.ex
@@ -91,7 +91,7 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   attr :id, :string, required: true
 
   def incident_card(assigns) do
-    IO.inspect(assigns, label: "incident_card assigns")
+    # IO.inspect(assigns, label: "incident_card assigns")
 
     ~H"""
     <.link navigate={~p"/incidents/#{@incident}"} id={@id}>
