09a880e08c5690494fea284217cc24c00c204c4d
Larry Jones <mrwizard82d1@gmail.com>
2025-10-03 13:56:25 -0500

    Ensure authentication on mount if needed

--- heads_up/lib/heads_up_web/live/admin_incident_live/index.ex
+++ heads_up/lib/heads_up_web/live/admin_incident_live/index.ex
@@ -4,6 +4,9 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
   alias HeadsUp.Admin
   import HeadsUpWeb.CustomComponents
 
+  on_mount {HeadsUpWeb.UserAuth, :ensure_authenticated}
+
+  # Run authorization checks
   def mount(_params, _session, socket) do
     socket =
       socket
@@ -26,7 +29,7 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
         Toggle Joke
       </.button>
       <div id="joke" class="joke hidden" phx-click={JS.toggle_class("blur")}>
-        Why shouldn't you trust trees?
+        Why shouldn't you trust trees, {@current_user.username}?
       </div>
       <.header>
         {@page_title}
