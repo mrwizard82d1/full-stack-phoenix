8287f50615d73d73fe41dc44a513a6f0fe65aa61
Larry Jones <mrwizard82d1@gmail.com>
2025-10-13 15:40:46 -0500

    Add form to submit a response

--- heads_up/lib/heads_up_web/live/incident_live/show.ex
+++ heads_up/lib/heads_up_web/live/incident_live/show.ex
@@ -4,7 +4,11 @@ defmodule HeadsUpWeb.IncidentLive.Show do
   alias HeadsUp.Incidents
   import HeadsUpWeb.CustomComponents
 
+  on_mount {HeadsUpWeb.UserAuth, :mount_current_user}
+
   def mount(_params, _session, socket) do
+    socket = assign(socket, :form, to_form(%{}))
+
     {:ok, socket}
   end
 
@@ -48,7 +52,28 @@ defmodule HeadsUpWeb.IncidentLive.Show do
         </section>
       </div>
       <div class="activity">
-        <div class="left"></div>
+        <div class="left">
+          <div :if={@incident.status == :pending}>
+            <%= if @current_user do %>
+              <.form for={@form} id="response-form">
+                <.input
+                  field={@form[:status]}
+                  type="select"
+                  prompt="Choose a status"
+                  options={[:enroute, :arrived, :departed]}
+                />
+
+                <.input field={@form[:note]} type="textarea" placeholder="Note..." autofocus />
+
+                <.button>Post</.button>
+              </.form>
+            <% else %>
+              <.link href={~p"/users/log-in"} class="button">
+                Log in to submit a response
+              </.link>
+            <% end %>
+          </div>
+        </div>
         <div class="right">
           <.urgent_incidents incidents={@urgent_incidents} />
         </div>
