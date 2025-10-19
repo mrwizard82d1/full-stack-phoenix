4aa83e4179237629f1b0d2295291542c005596d9
Larry Jones <mrwizard82d1@gmail.com>
2025-10-19 18:10:46 -0500

    Broadcast (admin) changes to raffles

--- raffley/lib/raffley/admin.ex
+++ raffley/lib/raffley/admin.ex
@@ -1,4 +1,5 @@
 defmodule Raffley.Admin do
+  alias Raffley.Raffles
   alias Raffley.Raffles.Raffle
   alias Raffley.Repo
 
@@ -44,6 +45,15 @@ defmodule Raffley.Admin do
     raffle
     |> Raffle.changeset(attrs)
     |> Repo.update()
+    |> case do
+      {:ok, raffle} ->
+        raffle = Repo.preload(raffle, :charity)
+        Raffles.broadcast(raffle.id, {:raffle_updated, raffle})
+        {:ok, raffle}
+
+      {:error, _} = error ->
+        error
+    end
   end
 
   def delete_raffle(%Raffle{} = raffle) do
--- raffley/lib/raffley_web/live/raffle_live/show.ex
+++ raffley/lib/raffley_web/live/raffle_live/show.ex
@@ -190,4 +190,8 @@ defmodule RaffleyWeb.RaffleLive.Show do
 
     {:noreply, socket}
   end
+
+  def handle_info({:raffle_updated, raffle}, socket) do
+    {:noreply, assign(socket, :raffle, raffle)}
+  end
 end
